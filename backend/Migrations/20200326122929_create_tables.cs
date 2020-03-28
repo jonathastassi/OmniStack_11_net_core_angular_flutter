710be46114712fc702ea976b2396dec1a77c26dc
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2020-03-28 12:30:33 -0300

    Criação das migrations

---
+++ backend/Migrations/20200326122929_create_tables.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class create_tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ongs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Whatsapp = table.Column<string>(type: "varchar(30)", nullable: false),
+                    City = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Uf = table.Column<string>(type: "varchar(2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ongs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incidents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Value = table.Column<decimal>(nullable: false),
+                    OngId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Incidents_Ongs_OngId",
+                        column: x => x.OngId,
+                        principalTable: "Ongs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incidents_OngId",
+                table: "Incidents",
+                column: "OngId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Incidents");
+
+            migrationBuilder.DropTable(
+                name: "Ongs");
+        }
+    }
+}
