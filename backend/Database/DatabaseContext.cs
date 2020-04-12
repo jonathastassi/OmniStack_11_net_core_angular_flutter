9f6bda5d330b61201f39b26664b064420ed91e2b
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2020-04-12 14:59:33 -0300

    DB e ajuste no context

--- backend/Database/DatabaseContext.cs
+++ backend/Database/DatabaseContext.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using backend.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace backend.Models
+namespace backend.Database
 {
     public class DatabaseContext : DbContext
     {
@@ -8,5 +12,10 @@ namespace backend.Models
 
         public DbSet<Ong> Ongs { get; set; }
         public DbSet<Incident> Incidents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
